9031aa7d2177367150f877b079df84e550f7516f
Rafael Paes <rfaelpaes@gmail.com>
2020-05-27 16:38:20 -0300

    Estrutura inicial

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Main from '~/pages/Main'
+
+const Stack = createStackNavigator()
+
+export default function Routes() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Main"
+      screenOptions={{headerShown: false}}
+    >
+      <Stack.Screen name="Main" component={Main} />
+    </Stack.Navigator>
+  )
+}
