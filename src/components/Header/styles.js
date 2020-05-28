567ea2bbbecdfa22717dd8b1071091658282241f
Rafael Paes <rfaelpaes@gmail.com>
2020-05-27 23:39:08 -0300

    :rocket: Componente Header usado para o topo

---
+++ src/components/Header/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+const Container = styled.View`
+  padding: 40px 0 30px;
+  align-items: center;
+`
+
+const Top = styled.View`
+  margin-bottom: 10px;
+  flex-direction: row;
+  align-items: center;
+`
+
+const Logo = styled.Image``
+
+const Title = styled.Text`
+  margin-left: 8px;
+  color: #FFF;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+export {
+  Container, Top, Logo, Title
+}
