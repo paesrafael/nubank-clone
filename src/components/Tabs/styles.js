7b726979ccfb86680a5fa0a33e726e4f4efdb7f8
Rafael Paes <rfaelpaes@gmail.com>
2020-05-27 23:44:24 -0300

    :rocket: Componente para as Tabs de icones

---
+++ src/components/Tabs/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+const Container = styled.View`
+  margin-top: 20px;
+  height: 100px;
+`
+
+const TabsContainer = styled.ScrollView.attrs({
+  horizontal: true,
+  contentContainerStyle: {
+    paddingRight: 10
+  },
+  showHorizontalScrollIndicator: false
+})``
+
+const TabItem = styled.View`
+  padding: 10px;
+  margin-left: 10px;
+  width: 100px;
+  height: 100px;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 3px;
+  justify-content: space-between;
+`
+
+const TabText = styled.Text`
+  font-size: 13px;
+  color: #FFF;
+`
+
+export {
+  Container, TabsContainer,
+  TabItem, TabText
+}
